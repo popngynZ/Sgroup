a6a9b48220b4017fd3a7bbc788ffbe5b0b4fbbd8
Trietlanhat <nguyenminhtrietdn02@gmail.com>
2023-01-06 18:27:19 +0700

    dap di xay lai

---
+++ Day/day10js/day10.js
@@ -0,0 +1,9 @@
+// const array =[1,2,3,4]
+// console.log (array["a"])
+// //forEach
+// //map
+// //filter
+// //find
+// //reduce
+
+// console.log (array.length [2])
---
+++ Day/day9js/ngay h/day9bt1.js
@@ -0,0 +1,12 @@
+let ngayGio = new Date();
+console.log(ngayGio);
+/*Lấy Các Thành Phần Trong Date()*/
+console.log("Năm Hiện Tại là: " + ngayGio.getFullYear());
+console.log("Tháng Hiện Tại là: " + ngayGio.getMonth());
+console.log("Ngày Hiện Tại là: " + ngayGio.getDate());
+console.log("Ngày Trong Tuần Hiện Tại là: " + ngayGio.getDay());
+console.log("Giờ Hiện Tại là: " + ngayGio.getHours());
+console.log("Phút Hiện Tại là: " + ngayGio.getMinutes());
+console.log("Giây Hiện Tại là: " + ngayGio.getSeconds());
+console.log("Mily Giây Hiện Tại là: " +ngayGio.getMilliseconds());
+console.log("Mốc Thời Gian Hiện Tại là: " + ngayGio.getTime());
